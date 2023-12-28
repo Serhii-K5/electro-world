9887f45704f0ab8f1ae3c7a6fff2537b7299bb16
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-12-28 14:43:17 +0200

    update

--- src/components/PriceRange1/PriceRange1.jsx
+++ src/components/PriceRange1/PriceRange1.jsx
@@ -14,68 +14,36 @@ import { changefilteredProducts } from 'redux/slice/filteredProductsSlice';
 
 
 const PriceRange1 = () => {
-  const shift = 20;
-  // const { JSDOM } = require('jsdom');
-  // const { window } = new JSDOM('');
-  // const $ = require('jquery')(window);
+  const SHIFT = 20;
   const dispatch = useDispatch();
   const filteredProducts = useSelector(selectFilteredProducts);
 
-  // const [rangePricePosition, setRangePosition] = useState(0);
   const [rangePriceWidth, setRangeWidth] = useState(0);
-  const [curentPosition1, setCurentPosition1] = useState(0);
-  const [curentPosition2, setCurentPosition2] = useState(100);
-  const [curentPositionLeft, setCurentPositionLeft] = useState(0);
-  const [curentPositionWidth, setCurentPositionWidth] = useState(100);
-  const [inputValue1, setInputValue1] = useState('');
-  const [inputValue2, setInputValue2] = useState('');
-  // const minPriceElement = document.getElementById('min-price');
-  // const maxPriceElement = document.getElementById('max-price');
+  const [curentPositionLeftMin, setCurentPositionLeftMin] = useState(0);
+  const [curentPositionLeftMax, setCurentPositionLeftMax] = useState(0);
+  
+  const [inputValueMin, setInputValueMin] = useState('');
+  const [inputValueMax, setInputValueMax] = useState('');
+
   const [maxPrice, setMaxPrice] = useState(0);
   const [minPrice, setMinPrice] = useState(0);  
-
+  
+  const [isVisable, setIsVisable] = useState(false);  
+  const [coordText, setCoordText] = useState('');  
+  
   if (filteredProducts.length === 0) {
     dispatch(changefilteredProducts(products1));
   }
-
-
-  // useEffect(() => {
-  //   // var coordinatesDisplay = document.getElementById('coords');
-  //   const rangePriceElement = document.getElementById('range-price');
-  //   const minPriceElement = document.getElementById('min-price');
-  //   const maxPriceElement = document.getElementById('max-price');
-    
-  //   const rangePositionX = rangePriceElement.clientX;
-  //   setRangePosition(rangePositionX);
-  //   const minPriceX = minPriceElement.clientX;
-  //   const maxPriceX = maxPriceElement.clientX;
-
-  //   setMaxPrice(findMaxPrice());
-  //   setMinPrice(findMinPrice());
-
-  //   const findMaxPrice = () => {
-  //     const max = filteredProducts.reduce(
-  //       (accumulator, currentValue) =>
-  //         (accumulator =
-  //           currentValue.price > accumulator
-  //             ? currentValue.price
-  //             : accumulator),
-  //       0
-  //     );
-      
-  //     // setInputValue2(max);
-
-  //     setInputValue2(Math.round((1-(rangePositionX + rangePriceElement.style.width - maxPriceX) / rangePriceElement.style.width)*100 - shift));
-
-  //     return max;
-  //   };
-
+  
   
   useEffect(() => {
     const rangePriceElement = document.getElementById('range-price');
     const rangeWidth = rangePriceElement.clientWidth;
     
     setRangeWidth(rangeWidth);
+    
+    setCurentPositionLeftMin(0);
+    setCurentPositionLeftMax(rangeWidth);
 
     const findMinMaxPrice = () => {
       const max = filteredProducts.reduce((accumulator, currentValue) => (
@@ -88,83 +56,108 @@ const PriceRange1 = () => {
         ), max
       );
       
-      setInputValue1(Math.round(min + (max - min) / 100 * curentPosition1));
-      setInputValue2(Math.round(min + (max - min) / 100 * curentPosition2)); 
-      setCurentPositionLeft(rangeWidth * curentPosition1 / 100);
-      setCurentPositionWidth(rangeWidth * curentPosition2 / 100);
+      setInputValueMin(min);
+      setInputValueMax(max);
+      
       setMaxPrice(max);
       setMinPrice(min);
     };
 
     findMinMaxPrice();
+    
   }, []);
   
-  const handleMouseEnter = e => {
-    // setRangePosition(e.target.clientX);
-    setRangeWidth(e.target.style.width);
-  };  
-
   const handleMoveMin = e => {
     // var x = e.curentTarget.clientX;
     // var y = e.clientY;
     // if (!(rangePricePosition < e.curentTarget.clientX)) {
-    //   setCurentPosition1(Math.round((e.curentTarget.clientX - rangePricePosition) / rangePriceWidth * 100)-shift);
+    //   setCurentPosition1(Math.round((e.curentTarget.clientX - rangePricePosition) / rangePriceWidth * 100)-SHIFT);
     // }
-
+    
     // e.target.style.left = x - e.target.clientWidth / 2 + 'px';
     // movableElement.style.top = y - movableElement.clientHeight / 2 + 'px';
-
+    
     // coordinatesDisplay.textContent = '(' + x + ', ' + y + ')';
   };
-
+  
   const handleMoveMax = e => {
     // !(rangePricePosition + rangePriceWidth < e.curentTarget.clientX) && 
-    //   setCurentPosition2(Math.round((1-(e.curentTarget.clientX - rangePricePosition) / rangePriceWidth) * 100)-shift);    
+    //   setCurentPosition2(Math.round((1-(e.curentTarget.clientX - rangePricePosition) / rangePriceWidth) * 100)-SHIFT);    
+  };
+  
+  const handleChangeMin = e => { 
+    const value = Number(e.target.value);
+    if (value < minPrice) {
+      setCurentPositionLeftMin(0);
+      setInputValueMin(minPrice);
+    } else {
+      const result = Math.round(rangePriceWidth * (value - minPrice) / maxPrice);
+      
+      if (result > curentPositionLeftMax) {
+        setCurentPositionLeftMin(curentPositionLeftMax);
+        setInputValueMin(inputValueMax);
+      } else {
+        setCurentPositionLeftMin(result);
+        setInputValueMin(value);
+      }
+    }
   };
 
-  const handleChange1 = e => {
-    const result = Math.round(rangePriceWidth * e.target.value / maxPrice);
-    setCurentPositionLeft(result);
-    setCurentPositionWidth((rangePriceWidth * (1-(result + curentPosition2 / 100))));
+  const handleChangeMax = e => {
+    const value = Number(e.target.value);
+    if (value > maxPrice) {
+      setCurentPositionLeftMax(maxPrice);
+      setInputValueMax(maxPrice);
+    } else {
+      const result = Math.round((rangePriceWidth * value) / maxPrice);
+      
+      if (result < curentPositionLeftMin) {
+        setCurentPositionLeftMax(curentPositionLeftMin);
+        setInputValueMax(inputValueMin);
+      } else {
+        setCurentPositionLeftMax(result);
+        setInputValueMax(value);
+      }
+    }
+  };
+  
+  // const onFocus = () => {
+    //   console.log('asrfh');
+    // }
     
+    const onChangeMin = e => {
+      const val = Math.abs(Number(e.target.value));
+      val > 0 && setInputValueMin(val);
+    };
     
-    // setInputValue1(Math.round(min + ((max - min) / 100) * curentPosition1));
-    // setInputValue2(Math.round(min + ((max - min) / 100) * curentPosition2));
-
-    // var x = e.clientX;
-    // // var y = e.clientY;
-
-    // e.target.style.left = x - e.target.clientWidth / 2 + 'px';
-    // // movableElement.style.top = y - movableElement.clientHeight / 2 + 'px';
-
-    // // coordinatesDisplay.textContent = '(' + x + ', ' + y + ')';
+    const onChangeMax = e => {
+      const val = Math.abs(Number(e.target.value));
+      val > 0 && setInputValueMax(val);
+    };
+    
+    const handleDrag = e => {
+      // setRangePosition(e.target.clientX);
+    setRangeWidth(e.target.style.width);
   };
-
-  const handleChange2 = e => {
-    var x = e.clientX;
-    // var y = e.clientY;
-
-    e.target.style.left = x - e.target.clientWidth / 2 + 'px';
-    // movableElement.style.top = y - movableElement.clientHeight / 2 + 'px';
-
-    // coordinatesDisplay.textContent = '(' + x + ', ' + y + ')';
+  
+  const handleMouseEnter = e => {
+    // setRangePosition(e.target.clientX);
+    setRangeWidth(e.target.style.width);
   };
+  
+  const onMouseLeave = e => {
+    // setCoordText('x=' + window.pageXOffset + '; y=' + window.pageYOffset);
+    // setCoordText('x=' + window.scrollX + '; y=' + window.scrollY);
+    setCoordText('x=' + e.clientX + '; y=' + e.clientY);
 
-  // const calcPosition1 = () => {
-  //   return Math.round(rangePriceWidth * curentPosition1 / 100) + 'px';
-  // };
-
-  // const calcPosition2 = () => {
-  //   return Math.round(rangePriceWidth * curentPosition2 / 100) + 'px';
-  // };
-
-
+    // setRangePosition(e.target.clientX);
+    // setRangeWidth(e.target.style.width);
+  };  
 
   return (
     <>
       <p>Цена</p>
       <div
-        // id="price-range"
         style={{
           display: 'flex',
           justifyContent: 'center',
@@ -175,29 +168,28 @@ const PriceRange1 = () => {
         <span>{'От '} </span>
         <input
           type="text"
-          // id="min-price"
           style={{
             width: '50px',
             padding: '7px',
             borderRadius: '5px',
-            borderColor: 'grey',
+            borderColor: 'var(--bg-color-grey)',
           }}
-          value={inputValue1}
-          onChange={handleChange1}
-
+          value={inputValueMin}
+          onChange={onChangeMin}
+          onBlur={handleChangeMin}
         />
         <span>{' до '}</span>
         <input
           type="text"
-          // id="max-price"
           style={{
             width: '70px',
             padding: '7px',
             borderRadius: '5px',
-            borderColor: 'grey',
+            borderColor: 'var(--bg-color-grey)',
           }}
-          value={inputValue2}
-          onChange={handleChange2}
+          value={inputValueMax}
+          onChange={onChangeMax}
+          onBlur={handleChangeMax}
         />
         <span>{' грн'}</span>
       </div>
@@ -206,7 +198,7 @@ const PriceRange1 = () => {
       <div
         style={{
           margin: '10px auto 0',
-          backgroundColor: 'blue',
+          backgroundColor: 'var(--bg-button-color)',
           color: 'white',
           width: '80px',
           height: '40px',
@@ -218,75 +210,86 @@ const PriceRange1 = () => {
       >
         OK
       </div>
-
+{/* ------------------------------------ */}
       <div
         style={{
-          display: 'flex',
+          // display: 'flex',
           position: 'relative',
-          justifyContent: 'center',
+          // justifyContent: 'center',
+          padding: '20px',
         }}
+        onMouseEnter={() => setIsVisable(true)}
+        onMouseOut={() => setIsVisable(false)}
+        // onMouseLeave={onMouseLeave}
+        onMouseMove={onMouseLeave}
       >
         <div
           id="range-price"
           style={{
-            width: `calc(100% - ${shift * 2}px)`,
-            height: '5px',
+            // width: `calc(100% - ${SHIFT * 2}px)`,
+            // width: '100%',
+            height: '4px',
             border: 'none',
-            backgroundColor: 'lightgrey',
-            marginTop: '20px',
+            backgroundColor: 'var(--bg-color-grey)',
           }}
-          onMouseEnter={handleMouseEnter}
+          // onMouseEnter={handleMouseEnter}
+          // onDrag={handleMouseEnter}
         ></div>
         <div
           style={{
-            // width: '50%',
-            // width: { curentPositionWidth } + 'px',
-            width: `${curentPositionWidth}px`,
-            height: '5px',
+            width: `${curentPositionLeftMax - curentPositionLeftMin}px`,
+            height: '4px',
             position: 'absolute',
             top: '20px',
-            left: `${curentPositionLeft + shift}px`,
+            // left: '20px',
+            left: `${curentPositionLeftMin + SHIFT}px`,
             border: 'none',
-            backgroundColor: 'blue',
+            backgroundColor: 'var(--bg-button-color)',
           }}
+          onMouseEnter={() => setIsVisable(true)}
         >
           <div
             id="min-price"
             style={{
-              width: '21px',
-              height: '21px',
-              // width: {shift + 1}'px',
-              // height: '21px',
+              width: '20px',
+              height: '20px',
               position: 'absolute',
-              top: '-8px',
-              left: `-${shift}px`,
-              // backgroundColor: 'blue',
+              // top: '-8px',
+              // left: `-${SHIFT}px`,
+              // position: 'relative',
+              top: '-9px',
+              left: '-20px',
               backgroundColor: 'white',
               borderRadius: '50%',
-              border: '2px solid blue',
-              boxShadow: '0 0 5px blue',
+              border: '2px solid var(--bg-button-color)',
+              boxShadow: '0 0 5px var(--bg-button-color)',
             }}
             // onMouseDown={handleMoveMin}
+            // onMouseEnter={handleMouseEnter}
+            // onMouseEnter={setIsVisable(true)}
+            // onMouseOut={setIsVisable(false)}
+            // onDrag={handleDrag}
+            // onDragStart={handleDrag}
+            onMouseEnter={() => setIsVisable(true)}
           ></div>
           <div
             id="max-price"
             style={{
-              width: '21px',
-              height: '21px',
+              width: '20px',
+              height: '20px',
               position: 'absolute',
-              top: '-8px',
-              right: `-${shift}px`,
-              // border: 'none',
-              // backgroundColor: 'blue',
-              backgroundColor: 'white',
+              top: '-9px',
+              right: `-${SHIFT}px`,
+              backgroundColor: 'var(--bg-primary)',
               borderRadius: '50%',
-              border: '2px solid blue',
-              boxShadow: '0 0 5px blue',
+              border: '2px solid var(--bg-button-color)',
+              boxShadow: '0 0 5px var(--bg-button-color)',
             }}
             // onMouseDown={handleMoveMax}
           ></div>
         </div>
       </div>
+      {isVisable && <div>{coordText}</div>}
     </>
   );
 };
