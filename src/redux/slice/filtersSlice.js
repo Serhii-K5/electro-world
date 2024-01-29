190c96acad4c810e28f261bce039503d24a974e8
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2024-01-29 16:35:43 +0200

    Update filtersSlice.js

--- src/redux/slice/filtersSlice.js
+++ src/redux/slice/filtersSlice.js
@@ -4,24 +4,12 @@ const sliceFilters = createSlice({
   name: 'filters',
   initialState: {
     items: [],
-    // items: [
-    //   {
-    //     key: 'name',
-    //     value: '',
-    //   },
-    //   {
-    //     key: 'price',
-    //     value: [[0, 0]],
-    //     // value: [{min: 0, max: 0}],
-    //   },
-    // ],
   },
   reducers: {
     changeFilters(state, action) {
       const arr = state.items.map(item => item.key);
       const index = arr.findIndex(item => item === action.payload.key);
       if (index < 0) {
-        // state.items.push(action.payload);
         state.items.push({ key: action.payload.key, value: [action.payload.value]});
       } else {
         if (action.payload.key === 'name' || action.payload.key === 'price' || action.payload.key === 'cat_parentId') {
@@ -38,26 +26,13 @@ const sliceFilters = createSlice({
       const arr = state.items.map(item => item.key);
       const index = arr.findIndex(item => item === action.payload.key);
       if (index >= 0) {
-        // if (state.items[index].value === '') {
         if (state.items[index].value.length > 1) {
           const pos = state.items[index].value.findIndex(item => item === action.payload.value);        
           state.items[index].value.splice(pos, 1);
-          // state.items.splice(index, 1); 
         } else {
           state.items.splice(index, 1);
-          // const pos = state.items[index].value.findIndex(item => item === action.payload.value);        
-          // state.items.value.splice(pos, 1);
         }
-        // console.log(state.items[index].value);
-
-        // const pos = [state.items[index].value].findIndex(item => item === console.log(item) && action.payload.value);
-        // const pos = state.items[index].value.findIndex(item => item === console.log(item) && action.payload.value);
-
-        // const pos = state.items[index].value.findIndex(item => item === action.payload.value);        
-        // state.items.value.splice(pos, 1);          
       }
-      // state.items.splice(0, state.items.length - 1);
-      // state.items.splice(0, 1, []);
     },
   },
 });
