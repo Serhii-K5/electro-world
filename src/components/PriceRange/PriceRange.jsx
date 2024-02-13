f9d39ab271b102bd888368124696ae075e930061
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2024-02-13 15:59:40 +0200

    Update PriceRange.jsx

--- src/components/PriceRange/PriceRange.jsx
+++ src/components/PriceRange/PriceRange.jsx
@@ -22,11 +22,9 @@ import lang from 'assets/json/language.json';
 
 import products1 from '../../assets/json/products.json';
 
-// const PriceRange = () => {
 const PriceRange = ({ data }) => {
   const dispatch = useDispatch();
   const filters = useSelector(selectFilters);
-  // const filteredProducts = useSelector(selectFilteredProducts);
   const filteredProducts = data.length > 0 ? data : products1;
   const languages = useSelector(selectLanguages);
 
@@ -44,42 +42,19 @@ const PriceRange = ({ data }) => {
   const [isMouseDownMin, setIsMouseDownMin] = useState(false);
   const [isMouseDownMax, setIsMouseDownMax] = useState(false);
 
-  // if (filteredProducts.length === 0) {
-  //   dispatch(changeFilteredProducts(products1));
-  // }
-
+  
   const changeFilter = (min, max) => {
     dispatch(changeFilters({ key: 'price', value: [min, max] }));
   };
 
   const findMinMaxPrice = () => {
-    // const max = filteredProducts.reduce(
-      //   (accumulator, currentValue) =>
-      //     (accumulator =
-      //       currentValue.price > accumulator ? currentValue.price : accumulator),
-      //   0
-      // );
-      
-      // const min = filteredProducts.reduce(
-        //   (accumulator, currentValue) =>
-        //     (accumulator =
-        //       currentValue.price < accumulator ? currentValue.price : accumulator),
-        //   max
-        // );
-        
     const [min, max] = searchMinMaxPrice(filteredProducts);
 
     setInputValueMin(min);
     setInputValueMax(max);
 
-    // const changeFilter = (min, max) => {
-    //   dispatch(changeFilters({ key: 'price', value: [min, max] }));
-    // };
-
     changeFilter(min, max);
     if (maxPrice !== max || minPrice !== min) {
-      // dispatch(changeFilters({ key: 'price', value: [min, max] }));
-      // console.log('findMinMaxPrice');
       changeFilter(min, max);
       if (minPrice !== min) setMinPrice(min);
       if (maxPrice !== max) setMaxPrice(max);
@@ -97,35 +72,23 @@ const PriceRange = ({ data }) => {
     findMinMaxPrice();
   }, []);
 
-  // useEffect(() => {
-  //   findMinMaxPrice();
-  //   console.log('data.length');
-  // // }, [data.length]);
-  // }, [filteredProducts]);
-
   useEffect(() => {
     const [min, max] = searchMinMaxPrice(filteredProducts);
     setInputValueMin(min);
     setInputValueMax(max);
-
   }, [filters]);
 
   useEffect(() => {
     const handleMouseMove = e => {
       if (isMouseDownMin) {
         const offsetMin = e.clientX - positionStart - SHIFT_RANGE * 0.5;
-        if (
-          offsetMin + SHIFT_RANGE * 0.5 >= positionStart &&
-          offsetMin <= positionMax
-        ) {
-          setInputValueMin(
-            Math.round((offsetMin * (maxPrice - minPrice)) / rangeWidth)
-          );
-          // setInputValueMin(Math.round(offsetMin * (inputValueMax - inputValueMin) / rangeWidth))
+        if (offsetMin >= 0 && offsetMin <= positionMax) {
+          setInputValueMin(Math.round((offsetMin * (maxPrice - minPrice)) / rangeWidth));
           setPositionMin(Math.round(offsetMin));
         } else if (offsetMin > positionMax) {
           setInputValueMin(inputValueMax);
-          setPositionMax(positionMax);
+          // setPositionMax(positionMax);
+          setPositionMin(positionMax);
         } else {
           setInputValueMin(minPrice);
           setPositionMin(0);
@@ -137,7 +100,6 @@ const PriceRange = ({ data }) => {
           setInputValueMax(
             Math.round((offsetMax * (maxPrice - minPrice)) / rangeWidth)
           );
-          // setInputValueMax(Math.round(offsetMax * (inputValueMax - inputValueMin) / rangeWidth))
           setPositionMax(Math.round(offsetMax));
         } else if (offsetMax < positionMin) {
           setInputValueMax(inputValueMin);
@@ -167,13 +129,11 @@ const PriceRange = ({ data }) => {
     if (value > inputValueMax) {
       setPositionMin(positionMax);
     } else if (value < minPrice) {
-      // } else if (value < inputValueMin) {
       setPositionMin(0);
     } else {
       const result = Math.round(
         (rangeWidth * (value - minPrice)) / (maxPrice - minPrice)
       );
-      // const result = Math.round(rangeWidth * (value - inputValueMin) / (inputValueMax - inputValueMin));
       setPositionMin(result);
     }
   };
@@ -181,11 +141,9 @@ const PriceRange = ({ data }) => {
   const handleChangeMin = e => {
     const value = Math.abs(Number(e.target.value));
     if (value >= minPrice && value <= inputValueMax) {
-      // if (value >= inputValueMin && value <= inputValueMax) {
       setInputValueMin(value);
     } else if (value >= inputValueMax) {
       setInputValueMin(maxPrice);
-      // setInputValueMin(inputValueMax);
     } else {
       setInputValueMin(minPrice);
     }
@@ -195,13 +153,11 @@ const PriceRange = ({ data }) => {
     if (value < inputValueMin) {
       setPositionMax(positionMin);
     } else if (value > maxPrice) {
-      // } else if (value > inputValueMax) {
       setPositionMax(rangeWidth);
     } else {
       const result = Math.round(
         (rangeWidth * (value - minPrice)) / (maxPrice - minPrice)
       );
-      // const result = Math.round(rangeWidth * (value - inputValueMin) / (inputValueMax - inputValueMin));
       setPositionMax(result);
     }
   };
@@ -209,7 +165,6 @@ const PriceRange = ({ data }) => {
   const handleChangeMax = e => {
     const value = Math.abs(Number(e.target.value));
     if (value >= inputValueMin && value <= maxPrice) {
-      // if (value >= inputValueMin && value <= inputValueMax){
       setInputValueMax(value);
     } else if (value < inputValueMin) {
       setInputValueMax(inputValueMin);
@@ -238,40 +193,24 @@ const PriceRange = ({ data }) => {
     }
   };
 
-  // const onChangeMinMax = e => {
-  //   const value = Math.abs(Number(e.target.value));
-  //   if (value >= 0) {
-  //     if (e.target.id === 'inputMin') {
-  //       setInputValueMin(value);
-  //       changePositionMin(value);
-  //     }
-  //     if (e.target.id === 'inputMax') {
-  //       setInputValueMax(value);
-  //       changePositionMax(value);
-  //     }
-  //   } else {
-  //     e.target.id === 'inputMin' && setInputValueMin(e.target.value);
-  //     e.target.id === 'inputMax' && setInputValueMax(e.target.value);
-  //   }
-  // };
-
   const handleMouseDownMin = () => {
     setIsMouseDownMin(true);
   };
+  
+  // const handleMouseUpMin = () => {
+  //   setIsMouseDownMin(false);
+  // };
 
-  // const handleMouseDownMax = e => {
   const handleMouseDownMax = () => {
     setIsMouseDownMax(true);
   };
 
+  // const handleMouseUpMax = () => {
+  //   setIsMouseDownMax(false);
+  // };
+
   const handleClickBtn = () => {
     changeFilter(inputValueMin, inputValueMax);
-
-    // // dispatch(deleteFilters({ key: 'price', value: [inputValueMin, inputValueMax] }));
-    // dispatch(deleteFilters({ key: 'price', value: [] }));
-
-    // // console.log("Button click");
-    // // alert("Button click");
   };
 
   return (
@@ -284,7 +223,6 @@ const PriceRange = ({ data }) => {
           type="text"
           style={{ width: '50px' }}
           value={inputValueMin}
-          // onChange={onChangeMinMax}
           onChange={onChangeMin}
           onBlur={handleChangeMin}
         />
@@ -294,14 +232,12 @@ const PriceRange = ({ data }) => {
           type="text"
           style={{ width: '70px' }}
           value={inputValueMax}
-          // onChange={onChangeMinMax}
           onChange={onChangeMax}
           onBlur={handleChangeMax}
         />
         <span>{' грн'}</span>
       </Form>
       <p></p>
-      {/* <button style={{ margin: '10px auto' }}>OK</button> */}
       <BtnDiv onClick={handleClickBtn}>
         <b>OK</b>
       </BtnDiv>
@@ -318,11 +254,13 @@ const PriceRange = ({ data }) => {
             id="min"
             style={{ left: `${-SHIFT_RANGE}px` }}
             onMouseDown={handleMouseDownMin}
+            // onMouseUp={handleMouseUpMin}
           />
           <RangeLineEdgesDiv
             id="max"
             style={{ right: `${-SHIFT_RANGE}px` }}
             onMouseDown={handleMouseDownMax}
+            // onMouseUp={handleMouseUpMax}
           />
         </RangeActiveDiv>
       </RangeContainer>
