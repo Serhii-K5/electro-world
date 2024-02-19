0d61b98562f638a38a85991856bdf4890b174ace
Serhii <kch_i@i.ua>
2024-02-19 22:11:01 +0200

    Update filtersSlice.js

--- src/redux/slice/filtersSlice.js
+++ src/redux/slice/filtersSlice.js
@@ -16,10 +16,14 @@ const sliceFilters = createSlice({
         state.items[existingFilterIndex].value.push(value);
       } else {
         // Если ключа нет, добавляем новый объект
-        // state.items.push({ key, value });
-        // const arr = [value];
-        // state.items.push({ key, arr });
-        state.items.push({ key: key, value: [value] });
+        // // state.items.push({ key, value });
+        // // const arr = [value];
+        // // state.items.push({ key, arr });
+        // state.items.push({ key: key, value: [value] });
+        const existingFilterValueIndex = state.items[existingFilterIndex].value.findIndex(filter => filter === value);
+        if (existingFilterValueIndex === -1) {
+          state.items.push({ key: key, value: [value] });
+        }
       }
     },
     changeFilters(state, action) {
@@ -32,9 +36,6 @@ const sliceFilters = createSlice({
         state.items[existingFilterIndex].value = [value];
       } else {
         // Если ключа нет, добавляем новый объект
-        // state.items.push({ key, value });
-        // const arr = [value];
-        // state.items.push({ key, arr });
         state.items.push({ key: key, value: [value] });
       }
     },
@@ -44,22 +45,12 @@ const sliceFilters = createSlice({
       // Проверяем, существует ли уже объект с таким ключом
       const existingFilterIndex = state.items.findIndex(filter => filter.key === key);
       if (existingFilterIndex !== -1) {
-        
-        // state.items.splice(existingFilterIndex, 1);
-        
-        // // if (state.items[index].value.length > 1) {
-        // //   const pos = state.items[index].value.findIndex(item => item === action.payload.value);        
-        // //   state.items[index].value.splice(pos, 1);
-        // // } else {
-        // //   state.items.splice(index, 1);
-        // // }
         const existingFilterValueIndex = state.items[existingFilterIndex].value.findIndex(filter => filter === value);
         if (existingFilterValueIndex !== -1) {
           state.items[existingFilterIndex].value.splice(existingFilterValueIndex, 1);
         } else {
           state.items.splice(existingFilterIndex, 1);
         }
-
       }
     },
   },
