d41cdf6ad4e48e2f03087835c8f0ff755a359416
Serhii <kch_i@i.ua>
2024-02-26 21:46:38 +0200

    Update Authorization.jsx

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,10 +1,14 @@
-
+import { useSelector } from "react-redux";
 import user from "assets/images/svg/user_icon.svg";
+import { selectLanguages } from 'redux/selectors';
+import lang from 'assets/json/language.json';
 
 const Authorization = () => {
+  const languages = useSelector(selectLanguages);
+
   return (
     <div style={{display: "flex", alignItems: 'center'}}>
-      <div>Авторизация</div>
+      <div>{lang[languages].Authorization}</div>
       <img src={user} alt="user icon" />
     </div>
 
