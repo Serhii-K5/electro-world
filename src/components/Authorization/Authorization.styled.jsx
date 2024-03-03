318730addb1901a44cdf97a99bc06c89d0baea51
Serhii <kch_i@i.ua>
2024-03-03 14:02:33 +0200

    Update Authorization.styled.jsx

--- src/components/Authorization/Authorization.styled.jsx
+++ src/components/Authorization/Authorization.styled.jsx
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
+import { transition } from 'components/ConstComponentsStyle/ConstComponentsStyle.styled';
 
 export const Div = styled.div`
   display: flex;
-  alignItems: center;
+  align-items: center;
   cursor: pointer;
+  color: inherit;
+  color: var(--text-color-white);
+  transition: color ${transition};
+
+  &:hover,
+  &:active,
+  &:focus {
+    color: var(--text-color-active-blue:);
+  };
 `;
 
 export const Img = styled.img`
