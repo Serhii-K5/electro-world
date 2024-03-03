f3397bfbb7eb548c0ece5a6b6a3ee7b97a57cebf
Serhii <kch_i@i.ua>
2024-03-03 22:25:07 +0200

    fixed bugs

--- src/components/FilterPanel/FilterPanel.jsx
+++ src/components/FilterPanel/FilterPanel.jsx
@@ -17,57 +17,82 @@ import shevron from "assets/images/svg/chevron-down.svg";
 import CreateMemoArray from "utilites/createMemoArray";
 
 
-
 const FilterPanel = ({ data }) => {
   const dispatch = useDispatch();
   const filters = useSelector(selectFilters);
   const [expanded, setExpanded] = useState({});
   const [filtersArray, setFiltersArray] = useState([]);
 
-  // useEffect(() => {
-  //   memoArray(filtersArray);
-  // }, []);
+  
+  const updateCheckboxes = () => {
+    setFiltersArray((prevFiltersArray) =>
+      prevFiltersArray.map((item) => ({
+        ...item,
+        values: item.values.map((value, index) => ({
+          ...value,
+          checked: toggleChecked(value.value),
+        })),
+      }))
+    );
+  };
 
   useEffect(() => {
     const memoArray = (arr) => {
-    data.map(item => {
-      const tempArray = CreateMemoArray(item.memo);
-      tempArray.flatMap(memoEl => {
-        if (memoEl && memoEl.key !== '') {
-          const keyArr = arr.map(item => item.key);
-          const index = keyArr.length > 0
-            ? keyArr.findIndex(memoKey =>
-              memoKey && memoEl.key.trim().toUpperCase() === memoKey.trim().toUpperCase()
-            )
-            : -1;
-          
-          if (index < 0) {
-            arr.push({ key: memoEl.key.trim(), values: [{ value: memoEl.value.trim(), count: 1 }] });
-          } else {
-            const pos = arr[index].values.findIndex(el => el.value === memoEl.value);
-            if (pos < 0) {
-              arr[index].values.push({ value: memoEl.value, count: 1 });
+      data.map(item => {
+        const tempArray = CreateMemoArray(item.memo);
+        tempArray.flatMap(memoEl => {
+          if (memoEl && memoEl.key !== '') {
+            const keyArr = arr.map(item => item.key);
+            const index = keyArr.length > 0
+              ? keyArr.findIndex(memoKey =>
+                memoKey && memoEl.key.trim().toUpperCase() === memoKey.trim().toUpperCase()
+              )
+              : -1;
+            
+            if (index < 0) {
+              arr.push({ key: memoEl.key.trim(), values: [{ value: memoEl.value.trim(), count: 1 }] });
             } else {
-              arr[index].values[pos].count++;
+              const pos = arr[index].values.findIndex(el => el.value === memoEl.value);
+              if (pos < 0) {
+                arr[index].values.push({ value: memoEl.value, count: 1 });
+              } else {
+                arr[index].values[pos].count++;
+              }
             }
           }
-        }
-        setFiltersArray(arr);
+          setFiltersArray(arr);
+          return 0;
+        });
         return 0;
       });
-      return 0;
-    });
-  };
+    };
 
     memoArray([]);
-  }, [data]);
+
+    const updateCheckboxes = () => {
+      setFiltersArray((prevFiltersArray) =>
+        prevFiltersArray.map((item) => ({
+          ...item,
+          values: item.values.map((value, index) => ({
+            ...value,
+            checked: toggleChecked(value.value),
+          })),
+        }))
+      );
+    };
+
+    updateCheckboxes();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, filters, expanded]);
 
   const changeCheckbox = (key, item, e) => {
     if (e.target.checked) {
-      dispatch(addFilters({ key: "memo", value: {key: key, value: item}}));
+      dispatch(addFilters({ key: "memo", value: item }));
     } else {
-      dispatch(deleteFilters({ key:"memo", value: {key: key, value: item}}));
+      dispatch(deleteFilters({ key:"memo", value: item }));
     }
+
+    updateCheckboxes(); 
   };
 
   const toggleDropdown = (key) => {
@@ -79,7 +104,7 @@ const FilterPanel = ({ data }) => {
 
   const toggleChecked = (item1) => {
     return filters.map(nameFilter => nameFilter.value).toString().includes(item1);
-  };
+  }
 
   return (
     <ul>
@@ -92,11 +117,16 @@ const FilterPanel = ({ data }) => {
             <Span>({values.length})</Span>
           </Div>
           <Ul style={{maxHeight: expanded[key] ? '100vh' : '0'}}>
-            {expanded[key] && values.map(({ value, count }, index) => ( 
-              // Отображаем количество элементов и их значения
-              <Li key={index}>
-                <input type="checkbox" id={`${key}-${index}`} value={value} defaultChecked={toggleChecked(value)} onClick={(e) => changeCheckbox(key, value, e)}/>
-                <label htmlFor={`${key}-${index}`}>{value} <Span>({count})</Span></label> {/* Отображаем количество элементов пункта */}
+            {expanded[key] && values.map(({ value, count, checked }, index) => (
+              <Li key={`${key}-${index}`}> 
+                <input
+                  type="checkbox"
+                  id={`${key}-${index}`}
+                  value={value}
+                  checked={checked}
+                  onChange={(e) => changeCheckbox(key, value, e)}
+                />
+                <label htmlFor={`${key}-${index}`}>{value} <Span>({count})</Span></label>
               </Li>
             ))}
           </Ul>
