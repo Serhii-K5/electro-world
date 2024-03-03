dbdd6bd39666c3d6a6fe328f1f969bdfbf54a71a
Serhii <kch_i@i.ua>
2024-03-03 13:56:27 +0200

    update style autorization

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -4,7 +4,7 @@ import user from "assets/images/svg/user_icon.svg";
 import { selectLanguages } from 'redux/selectors';
 import lang from 'assets/json/language.json';
 import AutorizationModule from "components/AutorizationModule/AutorizationModule";
-
+import { Div, Img } from "./Authorization.styled";
 
 const Authorization = () => {
   const languages = useSelector(selectLanguages);
@@ -21,10 +21,10 @@ const Authorization = () => {
 
   return (
     <>
-      <div onClick={onOpenModal} style={{display: "flex", alignItems: 'center', cursor: 'pointer'}}>
-        <div>{lang[languages].Authorization}</div>
-        <img src={user} alt="user icon" style={{width: '35px'}}/>
-      </div>
+      <Div onClick={onOpenModal} style={{display: "flex", alignItems: 'center', cursor: 'pointer'}}>
+        {lang[languages].Authorization}
+        <Img src={user} alt="user icon" style={{width: '35px'}}/>
+      </Div>
       {isModalShown && <AutorizationModule onClose={onCloseModal} />}
     </>
   );
--- src/components/Authorization/Authorization.styled.jsx
+++ src/components/Authorization/Authorization.styled.jsx
@@ -1,18 +1,11 @@
 import styled from 'styled-components';
 
-export const BgLogo = styled.div`
-  // position: relative;
-  background-repeat: no-repeat;
-  background-size: 100%;
-  border: 5px double orangered;
-  padding: 8px 16px;
-  border-radius: 50%;
-  // z-index: 50;
-  background-color: transporent;
+export const Div = styled.div`
+  display: flex;
+  alignItems: center;
+  cursor: pointer;
 `;
 
-export const TextLogo = styled.p`
-  font-size: 20px;
-  text-shadow: 1px 1px 0px rgba(255, 255, 255, 20);
-  color: #d96040;
+export const Img = styled.img`
+  width: 35px;
 `;
