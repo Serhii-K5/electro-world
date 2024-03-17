d6592453d01ea7a4a37dd21c0d7986ca6a7b5054
Serhii <kch_i@i.ua>
2024-03-17 18:18:01 +0200

    Update ProductCard.jsx

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -162,6 +162,7 @@ export default function Product({ card }) {
                 <ButtonDiv 
                   className={isOrder && "isOrder"}
                   onClick={handleClick}
+                  
                   // style={{ backgroundColor: isOrder ? "var(--bg-second-orange)" : "var(--bg-second-green)" }}
                 >
                   {isOrder ? 
