028c2b3cc680d2f95360197a9183dfd4732c132a
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2024-03-18 16:32:36 +0200

    Update Slider.jsx

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -4,7 +4,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import photo from "assets/images/jpg/productPhoto";
 
-import products from 'assets/json/products.json';
+// import products from 'assets/json/products.json';
 
 // import car1 from 'assets/images/auto/car-rent-1.jpg';
 // import car2 from 'assets/images/auto/car-rent-2.jpg';
@@ -12,12 +12,26 @@ import products from 'assets/json/products.json';
 // import car4 from 'assets/images/auto/car-rent-4.jpg';
 // import car5 from 'assets/images/auto/car-rent-5.jpg';
 
-const ProductsSlider = ({id, typePhoto}) => {
+const ProductsSlider = ({card, typePhoto}) => {
   
-  const index = products;
+  const photos = photo.map(item => item);
+  console.log(photos);
+  console.log(card);
+  
+  const slides = [];
+
+  const slidesArray = () => {
+    photos.map(item => {
+      return slides.push(item);
+    })
+  }
+  
+  console.log(slidesArray);
+
+  // const index = products;
   
   
-  const slides = [...date]; 
+  // const slides = [...date]; 
 
   
   class CarsSlider extends React.Component {
@@ -64,41 +78,6 @@ const ProductsSlider = ({id, typePhoto}) => {
               )
               
             )}
-            <div>
-              <img
-                src={car1}
-                alt="car 1"
-                style={{ width: '300px', height: '200px' }}
-              />
-            </div>
-            <div>
-              <img
-                src={car2}
-                alt="car 2"
-                style={{ width: '300px', height: '200px' }}
-              />
-            </div>
-            <div>
-              <img
-                src={car3}
-                alt="car 3"
-                style={{ width: '300px', height: '200px' }}
-              />
-            </div>
-            <div>
-              <img
-                src={car4}
-                alt="car 4"
-                style={{ width: '300px', height: '200px' }}
-              />
-            </div>
-            <div>
-              <img
-                src={car5}
-                alt="car 5"
-                style={{ width: '300px', height: '200px' }}
-              />
-            </div>
           </Slider>
         </div>
       );
