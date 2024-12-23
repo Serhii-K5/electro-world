7fca05db2acbed28c838ae54b5171705639d6ca8
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2024-12-23 08:25:57 +0200

    Update directoryPathSlice.js

--- src/redux/slice/directoryPathSlice.js
+++ src/redux/slice/directoryPathSlice.js
@@ -13,9 +13,9 @@ const sliceDirectoryPath = createSlice({
     // ],
   },
   reducers: {
-    addDirectoryPath(state, action) {
-      // state.items.push(action.payload);
-    },
+    // addDirectoryPath(state, action) {
+    //   // state.items.push(action.payload);
+    // },
     changeDirectoryPath(state, action) {
       const index = state.items.findIndex(item =>
         item.cat_id === action.payload.cat_parentId);
@@ -35,5 +35,6 @@ const sliceDirectoryPath = createSlice({
   },
 });
 
-export const { addDirectoryPath, changeDirectoryPath, deleteDirectoryPath, deleteAllDirectoryPath } = sliceDirectoryPath.actions;
+// export const { addDirectoryPath, changeDirectoryPath, deleteDirectoryPath, deleteAllDirectoryPath } = sliceDirectoryPath.actions;
+export const { changeDirectoryPath, deleteDirectoryPath, deleteAllDirectoryPath } = sliceDirectoryPath.actions;
 export const directoryPathReducer = sliceDirectoryPath.reducer;
